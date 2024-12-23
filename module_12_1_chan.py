20684ae8a5e9b9689d5480cca4000b199cf1f39d
santfan <santfan@mail.ru>
2024-12-23 20:51:25 +0300

    Тест кейсы с заморозкой

---
+++ module_12_1_chan.py
@@ -0,0 +1,47 @@
+# Импортируем класс из файла runner.py и нужный модуль
+import runner
+import unittest
+
+# Объявим класс тестера класса
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+    # Тестируем функцию walk
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        # Экземпляр класса Runner
+        walker = runner.Runner('name')
+        # Функцию запускаем 10 раз
+        for __ in range(10):
+            walker.walk()
+        # Сравниваем с эталоном
+        self.assertEqual(walker.distance, 50)
+
+    # Тестируем функцию run
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        # Экземпляр класса Runner
+        runner_ = runner.Runner('name')
+        # Функцию запускаем 10 раз
+        for __ in range(10):
+            runner_.run()
+        # Сравниваем с эталоном
+        self.assertEqual(runner_.distance, 100)
+
+    # Тестируем обе функции
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_chalenge(self):
+        # 2 экземпляра класса Runner
+        walker_1 = runner.Runner('name')
+        runner_1 = runner.Runner('name')
+        # Функцию запускаем 10 раз
+        for __ in range(10):
+            # Функция прогулки
+            walker_1.walk()
+            # Функция бегуна
+            runner_1.run()
+        # Результаты работы двух функций НЕ должны быть равны
+        self.assertNotEqual(walker_1.distance, runner_1.distance)
+
+# Входим в процедуру
+if __name__ == '__main__':
+    unittest.main()
---
+++ module_12_2_chan.py
@@ -0,0 +1,68 @@
+ # Импортируем модули runner_and_tournament и unittest
+import runner_and_tournament
+import unittest
+
+# Объявим класс тестирования
+class TournamentTest(unittest.TestCase):
+    is_frozen = True
+    # При первом запуске очистим all_results
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    # Для всех тест-сьютов сформируем экзмепляры бегунов
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def setUp(self):
+        self.usain = runner_and_tournament.Runner('Усейн', 10)
+        self.andrey = runner_and_tournament.Runner('Андрей', 9)
+        self.nik = runner_and_tournament.Runner('Ник', 3)
+
+    # Первый тест
+    # @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_1(self):
+        # Выбор участников забега
+        first_round = runner_and_tournament.Tournament(90, self.usain, self.nik)
+        # Старт забега
+        result = first_round.start()
+        list_runner = list(result.values())
+        # Проверка корректной работы
+        self.assertTrue(list_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+    # Второй тест
+    # @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_2(self):
+        # Выбор участников забега
+        second_round = runner_and_tournament.Tournament(90, self.andrey, self.nik)
+        # Старт забега
+        result = second_round.start()
+        list_runner = list(result.values())
+        # Проверка корректной работы
+        self.assertTrue(list_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+    # Третий тест
+    # @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_3(self):
+        # Выбор участников забега
+        third_round = runner_and_tournament.Tournament(90, self.usain, self.andrey, self.nik)
+        # Старт забега
+        result = third_round.start()
+        list_runner = list(result.values())
+        # Проверка корректной работы
+        self.assertTrue(list_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+    # Декоратор итогов тестирования
+    @classmethod
+    def tearDownClass(cls):
+        res = {}
+        for key, value in cls.all_results.items():
+            for key, value in value.items():
+                res[key] = str(value)
+            # Вывод итогов забега
+            print(res)
+
+# Точка входа в процедуру
+if __name__ == '__main__':
+    unittest.main()
